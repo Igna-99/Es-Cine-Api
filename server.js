cc576d590e5d3e502ef80c93873cd925da74214f
igna-99 <ignaciovarela7765@gmail.com>
2023-06-19 00:29:38 -0300

    handler de errores

--- server.js
+++ server.js
@@ -8,14 +8,21 @@ const app = express();
 //middleweres
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(indexRoutes)
+app.use(indexRoutes);
+
+// Error Handler
+app.use((error, req, res, next) => {
+    res
+        .status(error.status || 500)
+        .send({ success: false, message: error.message });
+});
 
 
 connection.sync({ force: true }).then(() => {
     app.listen(serverPort, () => {
         //console.clear()
-        console.log("server OK http://localhost:" + serverPort);    
+        console.log("server OK http://localhost:" + serverPort);
     })
-})
+});
