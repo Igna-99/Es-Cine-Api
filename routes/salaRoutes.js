4ed6dd8b754a1d000342034bedcc840e6a115eee
Camila Jaurena <jaurenacamila@gmail.com>
2023-06-27 23:36:22 -0300

    crear sala

--- routes/salaRoutes.js
+++ routes/salaRoutes.js
@@ -9,11 +9,7 @@ salaRoutes.get("/",salaController.traerTodasLasSalas);
 
 salaRoutes.get("/:sala",salaController.traerSala);
 
-<<<<<<< HEAD
 salaRoutes.post("/", salaController.crearSala);
 
-salaRoutes.put("/:sala", salaController.actualizarAsientos);
-=======
->>>>>>> f6a52bff816b9b4985c5530b5bf8b7c97eeef115
 
 export default salaRoutes;
